d879046573608ed75a5c8b2b9f1fd211d2154358
gelien0901 <201980292@psu.palawan.edu.ph>
2025-04-18 10:00:10 +0800

    Update views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -13,16 +13,6 @@ from django.db.models import Q
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-class ChartView(ListView):
-    template_name = 'chart.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
-    def get_queryset(self, *args, **kwargs):
-        pass
-
     
 # Organization URLs
 

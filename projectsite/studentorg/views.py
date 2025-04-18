dcaf641c72a8db8493152624a0f604d301e1e78d
gelien0901 <201980292@psu.palawan.edu.ph>
2025-04-18 09:53:53 +0800

    Update chart.html

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -12,6 +12,17 @@ from django.db.models import Q
 @method_decorator(login_required, name='dispatch')
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
     
 # Organization URLs
 

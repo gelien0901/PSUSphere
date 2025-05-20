ab2af6fdc6dd362ad4d0bcd9ebe9535e749a1ce1
gellen0901 <201980292@psu.palawan.edu.ph>
2025-05-20 20:23:07 +0800

    update charts

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path
+from studentorg import views
 from studentorg.views import (
     HomePageView,
     OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView,
@@ -9,6 +10,7 @@ from studentorg.views import (
     ProgramList, ProgramCreateView, ProgramUpdateView, ProgramDeleteView,
 )
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +26,7 @@ urlpatterns = [
     re_path(r'^login/$', auth_views.LoginView.as_view(
     template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    
+
     # Organization member URLs
     path('orgMember_list/', OrgMemberList.as_view(), name='orgMem_list'),
     path('orgMember_list/add/', OrgMemberCreateView.as_view(), name='orgMem-add'),
@@ -49,5 +51,15 @@ urlpatterns = [
     path('program_list/add/', ProgramCreateView.as_view(), name='program-add'),
     path('program_list/<int:pk>/', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<int:pk>/delete/', ProgramDeleteView.as_view(), name='program-delete'),
+
     
+    # Render the page
+    path('charts/', views.chart_page, name='chart-page'),
+
+# Provide JSON data
+    path('chart-data/', views.chart_data, name='chart-data'),
+    path('scatter-chart-data/', views.scatter_chart_data, name='scatter-chart-data'),
+    path('program-chart-data/', views.program_chart_data, name='program-chart-data'),
+    path('member_joined_by_month/', views.member_joined_by_month, name='member-joined-by-month'),
+    path('student_heatmap_data/', views.get_student_heatmap_data, name='student_heatmap_data'),
 ]
